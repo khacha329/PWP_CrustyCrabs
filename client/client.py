72d39c4e70e858ef19f2b97d6576756e94e80489
talha-zshan <talhazshan@gmail.com>
2024-05-15 19:18:33 +0300

    client timeout error handling

--- client/client.py
+++ client/client.py
@@ -11,28 +11,23 @@ TODO's:
 - ask user for image or path to image that is sent to aux api and returns information
 - Clear previous stock details windows when you use 'back' button
 """
+
 import curses
 from curses import wrapper
 import requests
-from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
+from requests.exceptions import Timeout, RequestException
 from error import APIError
 from util import menu, ask_inputs, display_dict, display_nested_dict
+from client_constants import INVENTORY_MANAGER_API, AUX_API, NAMESPACE, TIMEOUT_DURATION
 
-
-INVENTORY_MANAGER_API = "http://localhost:5000"
-AUX_API = "http://localhost:5001"
-NAMESPACE = "invmanager"
 
 def main(stdscr):
     """
-    Main function 
+    Main function
 
-    :param stdscr: _description_
-    :raises APIError: _description_
-    :raises APIError: _description_
-    :return: _description_
+    :param stdscr: The curses window object for displaying messages.
     """
-#69
+    # 69
 
     stdscr.clear()
     stdscr.addstr(0, 0, "Handheld Client:", curses.A_BOLD)
@@ -48,16 +43,20 @@ def main(stdscr):
 
         if selected_option == "Enter Stock":
             try:
-                warehouse_id = list(ask_inputs(user_entry_window, ["Enter Warehouse ID: "]))[0]
+                warehouse_id = list(
+                    ask_inputs(user_entry_window, ["Enter Warehouse ID: "])
+                )[0]
                 warehouse_id = int(warehouse_id)
-                item_name = list(ask_inputs(user_entry_window, ["Enter Item Name: "]))[0]
+                item_name = list(ask_inputs(user_entry_window, ["Enter Item Name: "]))[
+                    0
+                ]
             except ValueError:
                 stdscr.addstr(20, 0, "Invalid warehouse ID. Must be a number.")
                 stdscr.refresh()
                 continue
 
         elif selected_option == "Scan Stock":
-            #ask user for image or path to image that is sent to aux api and returns information
+            # ask user for image or path to image that is sent to aux api and returns information
 
             warehouse_id, item_name = scan_stock(stdscr, user_entry_window)
 
@@ -65,46 +64,96 @@ def main(stdscr):
             break
 
         while True:
-            #new menu to interact with stock
+            # new menu to interact with stock
             menu_title = f"Selected {item_name} in warehouse {warehouse_id}"
-            stock_response, stock_response_clean, current_quantity = get_stock(warehouse_id, item_name)
-            display_dict(stock_window, stock_response_clean, title = "STOCK")
+            stock_response, stock_response_clean, current_quantity = get_stock(
+                warehouse_id, item_name
+            )
+            display_dict(stock_window, stock_response_clean, title="STOCK")
 
             # Scan QR code as option? in addition to Print QR code
-            selected_option = menu(menu_window, ["Change Quantity", "Change Price", "Print QR Code","View Item Info", "Item-Stock in other Warehouses","Back"], menu_title=f"Selected {item_name} in warehouse {warehouse_id}")
+            selected_option = menu(
+                menu_window,
+                [
+                    "Change Quantity",
+                    "Change Price",
+                    "Print QR Code",
+                    "View Item Info",
+                    "Item-Stock in other Warehouses",
+                    "Back",
+                ],
+                menu_title=f"Selected {item_name} in warehouse {warehouse_id}",
+            )
             if selected_option == "Change Quantity":
-                quantity_option = menu(menu_window, ["Add 1", "Add 5", "Remove 1", "Remove 5", "Enter Custom Amount", "Back"], menu_title="Update Stock Quantity")
-                if quantity_option.startswith("Add") or quantity_option.startswith("Remove"):
-                    current_quantity = modify_quantity(stdscr, warehouse_id, item_name, quantity_option, current_quantity)
+                quantity_option = menu(
+                    menu_window,
+                    [
+                        "Add 1",
+                        "Add 5",
+                        "Remove 1",
+                        "Remove 5",
+                        "Enter Custom Amount",
+                        "Back",
+                    ],
+                    menu_title="Update Stock Quantity",
+                )
+                if quantity_option.startswith("Add") or quantity_option.startswith(
+                    "Remove"
+                ):
+                    current_quantity = modify_quantity(
+                        stdscr,
+                        warehouse_id,
+                        item_name,
+                        quantity_option,
+                        current_quantity,
+                    )
                 elif quantity_option == "Enter Custom Amount":
                     try:
-                        custom_amount_input = next(ask_inputs(user_entry_window, ["Enter Custom Amount: "]))
+                        custom_amount_input = next(
+                            ask_inputs(user_entry_window, ["Enter Custom Amount: "])
+                        )
                         custom_amount = int(custom_amount_input)
-                        action_type = menu(menu_window, ["Add", "Remove"], menu_title="Add or Remove?")
-                        current_quantity = modify_quantity(stdscr, warehouse_id, item_name, f"{action_type} {custom_amount}", current_quantity)
+                        action_type = menu(
+                            menu_window, ["Add", "Remove"], menu_title="Add or Remove?"
+                        )
+                        current_quantity = modify_quantity(
+                            stdscr,
+                            warehouse_id,
+                            item_name,
+                            f"{action_type} {custom_amount}",
+                            current_quantity,
+                        )
                     except StopIteration:
                         stdscr.addstr(20, 0, "No amount entered.")
                     except ValueError:
-                        stdscr.addstr(20, 0, "Invalid input. Please enter a valid number.")
+                        stdscr.addstr(
+                            20, 0, "Invalid input. Please enter a valid number."
+                        )
                 stdscr.refresh()
             elif selected_option == "Change Price":
                 try:
-                    price_input = next(ask_inputs(user_entry_window, ["Enter New Price: "]))
+                    price_input = next(
+                        ask_inputs(user_entry_window, ["Enter New Price: "])
+                    )
                     new_price = float(price_input)
-                    update_price(stdscr, warehouse_id, item_name, current_quantity, new_price)
+                    update_price(
+                        stdscr, warehouse_id, item_name, current_quantity, new_price
+                    )
                 except StopIteration:
                     stdscr.addstr(20, 0, "No price entered.")
                 except ValueError:
                     stdscr.addstr(20, 0, "Invalid input. Please enter a valid number.")
                 stdscr.refresh()
             elif selected_option == "Print QR Code":
-                #Maybe return path to QR code. scan QR code and return image path, open image popup 
+                # Maybe return path to QR code. scan QR code and return image path, open image popup
                 print_qr_code(stdscr, warehouse_id, item_name)
             elif selected_option == "View Item Info":
                 view_item_info(stdscr, stock_item_window, item_name)
             elif selected_option == "Item-Stock in other Warehouses":
                 item_stock_response = follow_relation(stock_response, "stock-item-all")
-                display_nested_dict(stock_item_window, item_stock_response["items"], title = "ITEM-STOCK")
+                display_nested_dict(
+                    stock_item_window, item_stock_response["items"], title="ITEM-STOCK"
+                )
             elif selected_option == "Back":
                 menu_window.clear()
                 stdscr.refresh()
@@ -119,13 +168,25 @@ def get_stock(warehouse_id, item_name):
     :return: _description_
     """
     # get stock of item in warehouse
-
-    stock_response = requests.get(INVENTORY_MANAGER_API + f"/api/stocks/{warehouse_id}/item/{item_name}/").json()
-    stock_quantity = stock_response['quantity']
-    stock_shelf_price = stock_response['shelf_price']
+    try:
+        stock_response = requests.get(
+            INVENTORY_MANAGER_API + f"/api/stocks/{warehouse_id}/item/{item_name}/",
+            timeout=TIMEOUT_DURATION,
+        ).json()
+    except Timeout:
+        print(f"The request timed out after {TIMEOUT_DURATION} seconds")
+        stock_response = None
+        return None
+    except RequestException as e:
+        print(f"An error occurred while carrying out the request: {e}")
+        stock_response = None
+        return None
+    stock_quantity = stock_response["quantity"]
+    stock_shelf_price = stock_response["shelf_price"]
     stock_response_clean = {k: v for k, v in stock_response.items() if "@" not in k}
     NAMESPACE = list(stock_response["@namespaces"].keys())[0]
     return stock_response, stock_response_clean, stock_quantity
+
 
 def get_item_id_by_name(warehouse_id, item_name):
     """
@@ -136,21 +197,25 @@ def get_item_id_by_name(warehouse_id, item_name):
     """
     try:
         url = f"{INVENTORY_MANAGER_API}/api/stocks/{warehouse_id}/item/{item_name}/"
-        response = requests.get(url)
+        response = requests.get(url=url, timeout=TIMEOUT_DURATION)
         response.raise_for_status()
         item_data = response.json()
 
-        if 'item_id' in item_data:
-            return item_data['item_id']
+        if "item_id" in item_data:
+            return item_data["item_id"]
         else:
             print(f"No 'item_id' found in the response for item: {item_name}")
             return None
+    except Timeout:
+        print(f"Error! Request Timed Out after {TIMEOUT_DURATION} seconds")
+        return None
     except requests.HTTPError as e:
         print(f"HTTP Error: {e.response.status_code} for item: {item_name}")
         return None
     except requests.RequestException as e:
         print(f"Request failed: {str(e)}")
         return None
+
 
 def modify_quantity(stdscr, warehouse_id, item_name, action, current_quantity):
     """
@@ -171,6 +236,7 @@ def modify_quantity(stdscr, warehouse_id, item_name, action, current_quantity):
     modified_quantity = quantity
     return modified_quantity
 
+
 def update_stock(stdscr, warehouse_id, item_name, quantity):
     """
     Updates the stock quantity for a given item in a warehouse.
@@ -188,20 +254,23 @@ def update_stock(stdscr, warehouse_id, item_name, quantity):
         return
 
     url = f"{INVENTORY_MANAGER_API}/api/stocks/{warehouse_id}/item/{item_name}/"
-    data = {'warehouse_id': warehouse_id, 'item_id': item_id, 'quantity': quantity}
-    
+    data = {"warehouse_id": warehouse_id, "item_id": item_id, "quantity": quantity}
+
     try:
-        response = requests.put(url, json=data)
+        response = requests.put(url, json=data, timeout=TIMEOUT_DURATION)
         if response.status_code in {200, 204}:
             stdscr.addstr(20, 0, "Stock updated successfully.")
         else:
             raise APIError(response.status_code, response.text, url)
+    except Timeout as t:
+        stdscr.addstr(20, 0, f"Request Timed Out: {str(t)}")
     except requests.RequestException as e:
         stdscr.addstr(20, 0, f"Network or request issue: {str(e)}")
     except APIError as api_error:
         error_message = str(api_error) if str(api_error) else "Unknown API error."
         stdscr.addstr(20, 0, error_message)
     stdscr.refresh()
+
 
 def update_price(stdscr, warehouse_id, item_name, modifeid_quantity, new_price):
     """
@@ -220,14 +289,22 @@ def update_price(stdscr, warehouse_id, item_name, modifeid_quantity, new_price):
         return
 
     url = f"{INVENTORY_MANAGER_API}/api/stocks/{warehouse_id}/item/{item_name}/"
-    data = {'warehouse_id': warehouse_id, 'item_id': item_id, 'quantity': modifeid_quantity, 'shelf_price': new_price} #need to somehow pass quantity with data
+    data = {
+        "warehouse_id": warehouse_id,
+        "item_id": item_id,
+        "quantity": modifeid_quantity,
+        "shelf_price": new_price,
+    }  # need to somehow pass quantity with data
 
     try:
-        response = requests.put(url, json=data)
+        response = requests.put(url, json=data, timeout=TIMEOUT_DURATION)
         if response.status_code in {200, 204}:
             stdscr.addstr(20, 0, "Price updated successfully.")
         else:
             raise APIError(response.status_code, response.text, url)
+        stdscr.refresh()
+    except Timeout as t:
+        stdscr.addstr(20, 0, f"Request TimedOut: {str(t)}")
         stdscr.refresh()
     except requests.RequestException as e:
         stdscr.addstr(20, 0, f"Network or request issue: {str(e)}")
@@ -237,40 +314,79 @@ def update_price(stdscr, warehouse_id, item_name, modifeid_quantity, new_price):
         stdscr.addstr(20, 0, error_message)
         stdscr.refresh()
 
+
 def scan_stock(stdscr, user_entry_window):
+    """
+    Scans the current stock.
+
+    :param stdscr: The curses window object for displaying messages.
+    :param user_entry_window: Window used by the user to interact with client.
+    """
     inputs = list(ask_inputs(user_entry_window, ["Enter path to QR image: "]))
     image_path = inputs[0] if inputs else None
     try:
-        with open(image_path, 'rb') as file:
-            files = {'image': file}
-            response = requests.post(f"{AUX_API}/api/qrRead/", files=files)
+        with open(image_path, "rb") as file:
+            files = {"image": file}
+            response = requests.post(
+                f"{AUX_API}/api/qrRead/", files=files, timeout=TIMEOUT_DURATION
+            )
             response.raise_for_status()
             stock_info = response.json()
-            return stock_info['warehouse_id'], stock_info['item_name']
+            return stock_info["warehouse_id"], stock_info["item_name"]
+    except Timeout as t:
+        stdscr.addstr(20, 0, f"Request TimedOut: {str(t)}")
+        stdscr.refresh()
+        return None, None
     except requests.RequestException as e:
         stdscr.addstr(20, 0, f"Failed to scan stock: {str(e)}")
         stdscr.refresh()
         return None, None
 
+
 def print_qr_code(stdscr, warehouse_id, item_name):
-    response = requests.get(f"{AUX_API}/api/qrGenerate/?warehouse_id={warehouse_id}&item_name={item_name}")
-    if response.status_code == 200:
-        with open('output_qr.png', 'wb') as f:
-            f.write(response.content)
-        stdscr.addstr(20, 0, "QR Code saved as 'output_qr.png'.")
+    """
+    Print the QR-Code.
+
+    :param stdscr: The curses window object for displaying messages.
+    :param warehouse_id: ID of the warehouse where the item is stored.
+    :param item_name: Name of the item to update.
+    """
+    try:
+        response = requests.get(
+            f"{AUX_API}/api/qrGenerate/?warehouse_id={warehouse_id}&item_name={item_name}",
+            timeout=TIMEOUT_DURATION,
+        )
+        if response.status_code == 200:
+            with open("output_qr.png", "wb") as f:
+                f.write(response.content)
+            stdscr.addstr(20, 0, "QR Code saved as 'output_qr.png'.")
+            stdscr.refresh()
+        else:
+            stdscr.addstr(20, 0, f"Failed to print QR code: {response.text}")
+            stdscr.refresh()
+    except Timeout as t:
+        stdscr.addstr(20, 0, f"Request TimedOut: {str(t)}")
         stdscr.refresh()
-    else:
-        stdscr.addstr(20, 0, f"Failed to print QR code: {response.text}")
+    except requests.RequestException as e:
+        stdscr.addstr(20, 0, f"Failed to print QR Code: {str(e)}")
         stdscr.refresh()
 
+
 def view_item_info(stdscr, stock_window, item_name):
-    """_summary_
+    """View the info of a given item
 
     :param stdscr: _description_
     :param stock_window: _description_
     :param item_name: _description_
     """
-    response = requests.get(f"{INVENTORY_MANAGER_API}/api/items/{item_name}/")
+    try:
+        response = requests.get(
+            url=f"{INVENTORY_MANAGER_API}/api/items/{item_name}/",
+            timeout=TIMEOUT_DURATION,
+        )
+    except Timeout as t:
+        stdscr.addstr(20, 0, f"Request Timed Out: {str(t)}")
+        stdscr.refresh()
     response_clean = {k: v for k, v in response.json().items() if "@" not in k}
     if response.status_code == 200:
         item_details = response_clean
@@ -279,9 +395,23 @@ def view_item_info(stdscr, stock_window, item_name):
         stdscr.addstr(20, 0, "Failed to retrieve item details.")
         stdscr.refresh()
 
+
 def follow_relation(response, relation):
+    """Follow the link relation in the given response
+
+    :param response: The API response
+    :param relation: The link relation that needs to be followed
+    """
     relation_url = response["@controls"][f"{NAMESPACE}:{relation}"]["href"]
-    return requests.get(INVENTORY_MANAGER_API + relation_url).json()
+    try:
+        return requests.get(
+            INVENTORY_MANAGER_API + relation_url, timeout=TIMEOUT_DURATION
+        ).json()
+    except Timeout:
+        print("Request Timed Out")
+        return None
+    # return requests.get(INVENTORY_MANAGER_API + relation_url).json()
+
 
 if __name__ == "__main__":
     wrapper(main)
---
+++ client/client_constants.py
@@ -0,0 +1,4 @@
+INVENTORY_MANAGER_API = "http://localhost:5000"
+AUX_API = "http://localhost:5001"
+NAMESPACE = "invmanager"
+TIMEOUT_DURATION = 30
